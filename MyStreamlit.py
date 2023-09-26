cb596d4162a578bab986b7d06b19c6b8809145be
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-26 15:02:05 +0700

    .commit

--- MyStreamlit.py
+++ MyStreamlit.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 st.image('./pic/kairung.jfif')
-
-st.header('Kairung Hengpraprohm')
+col1, col2 = st.columns(2)
+with col1:
+  st.header('ไก้รุ่ง เฮงพระพรหม')
+with col2:
+  st.subheader('สาขาวิชาวิทยาการข้อมูล')
+  st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
