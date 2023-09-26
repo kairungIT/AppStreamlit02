4be84fedb85057dc43261097244a3e9188adb74b
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-26 15:07:30 +0700

    .commit

--- MyStreamlit.py
+++ MyStreamlit.py
@@ -7,3 +7,11 @@ with col1:
 with col2:
   st.subheader('สาขาวิชาวิทยาการข้อมูล')
   st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
+
+  html_1 = """
+<div style="background-color:#76D7C4;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h4>การทำนายข้อมูลดอกไม้เบื้องต้น</h4></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
